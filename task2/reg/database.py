2e1c683cd8655d6482e22a99e9ae17574464aaa0
Rad1o4kt1v <abraevradmir2@gmail.com>
2025-07-13 20:22:52 +0500

    Delete task2/reg/database.py

--- task2/reg/database.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, create_engine, Session
-
-DATABASE_URL = "postgresql://dtc_clean:12345pass@localhost:5432/dtc_clean_db"
-engine = create_engine(DATABASE_URL, echo=True)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
