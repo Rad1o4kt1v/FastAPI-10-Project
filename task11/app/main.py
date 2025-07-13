a53aef7f697e7a47ce9ca83d107e1f33f916d912
Rad1o4kt1v <abraevradmir2@gmail.com>
2025-07-13 20:27:49 +0500

    Delete task11/app/main.py

--- task11/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from .tasks import send_email
-
-app = FastAPI()
-
-@app.get("/send-email/{email}")
-def trigger_email(email: str):
-    send_email.delay(email)
-    return {"message": "Email sending started"}
