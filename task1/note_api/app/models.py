852e398005cae8155934063488d671519f7f3c3b
Rad1o4kt1v <abraevradmir2@gmail.com>
2025-07-13 20:26:06 +0500

    Delete task1/note_api/app/models.py

--- task1/note_api/app/models.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
-from datetime import datetime
-
-class Note(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    text: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
