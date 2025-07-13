db66b032c0ee562b072ed701b481aaed33ac57c2
Rad1o4kt1v <abraevradmir2@gmail.com>
2025-07-13 20:27:56 +0500

    Delete task11/app/tasks.py

--- task11/app/tasks.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from .celery_worker import celery
-
-@celery.task
-def send_email(to_email: str):
-    print(f"Start sending email to {to_email}")
-    sleep(5)
-    print(f"Email sent to {to_email}")
-    return f"Sent to {to_email}"
